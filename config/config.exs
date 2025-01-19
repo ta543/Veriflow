afc4a58782d4a59237b0ffccb8cbdf6d1ee39085
Tobias Andersen <chappymusicc@gmail.com>
2025-01-19 04:11:48 +0100

    f

--- config/config.exs
+++ config/config.exs
@@ -1,7 +1,9 @@
 import Config
 
+# General application configuration
 config :veriflow, ecto_repos: [VeriFlow.Repo]
 
+# Endpoint configuration
 config :veriflow, VeriFlowWeb.Endpoint,
   url: [host: "localhost"],
   secret_key_base: "NOS4Q4GRixHDQqbu6bcvrTJj4Ye5T5PYychv+qTOHpKhSiCejhTKijY3whi9pqoU",
@@ -18,49 +20,40 @@ config :veriflow, VeriFlowWeb.Endpoint,
       ~r"assets/static/.*(svg|eot|woff|woff2|ttf)$"
     ]
   ],
-  http: [ip: {127, 0, 0, 1}, port: 4000]
+  http: [ip: {127, 0, 0, 1}, port: 4001]
 
+# Phoenix JSON library
 config :phoenix, :json_library, Jason
 
+# Wallaby configuration
 config :wallaby,
   driver: Wallaby.Chrome,
   chromedriver: [path: "/opt/homebrew/bin/chromedriver"]
 
+# Logger configuration
 config :logger, :console,
   format: "$time $metadata[$level] $message\n",
   metadata: [:request_id]
 
-config :veriflow, :plug_cowboy,
-  http: [ip: {127, 0, 0, 1}, port: 4000]
+# PubSub Configuration with Redis
+config :veriflow, VeriFlow.PubSub,
+  adapter: Phoenix.PubSub.Redis,
+  redis_url: "redis://localhost:6379/0"
 
+# Gettext configuration
 config :veriflow, VeriFlowWeb.Gettext,
   default_locale: "en",
-  locales: ~w(en fr es) # Add your supported locales here
+  locales: ~w(en fr es)
 
 config :veriflow, VeriFlowWeb.Endpoint,
-  live_reload: [
-    patterns: [
-      ~r"lib/veriflow_web/views/.*(ex)$", 
-      ~r"lib/veriflow_web/templates/.*(eex)$",
-      ~r"priv/gettext/.*(po)$",
-      ~r"assets/js/.*(js)$",
-      ~r"assets/css/.*(css)$",
-      ~r"assets/static/.*(svg|eot|woff|woff2|ttf)$"
-      ]
-  ]
+watchers: [
+  tailwind: {Tailwind, :install_and_run, [:veriflow, ["--watch"]]}
+]
 
-config :veriflow, VeriFlow.PubSub,
-adapter: Phoenix.PubSub.Redis,
-redis_url: "redis://localhost:6379/0"
-
-config :veriflow, VeriFlowWeb.ErrorView,
-  view: VeriFlowWeb.ErrorView,
-  accepts: ~w(html json),
-  layout: false
-
+# Tailwind configuration
 config :tailwind,
   version: "3.4.6",
-  veriflow: [ 
+  veriflow: [
     args: ~w(
       --config=tailwind.config.js
       --input=css/app.css
@@ -69,12 +62,14 @@ config :tailwind,
     cd: Path.expand("../assets", __DIR__)
   ]
 
+# Esbuild configuration
 config :esbuild,
-  version: "0.23.0", 
-  veriflow: [ 
+  version: "0.23.0",
+  veriflow: [
     args: ~w(js/app.js --bundle --target=es2016 --outdir=../priv/static/assets),
     cd: Path.expand("../assets", __DIR__),
     env: %{"NODE_PATH" => Path.expand("../deps", __DIR__)}
   ]
 
+# Import environment-specific configurations
 import_config "#{config_env()}.exs"
